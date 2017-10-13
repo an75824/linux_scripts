75c7edf1f90e49d4a4c5cba5e032f576a88ce7a9
antony13 <nanosa@uni.coventry.ac.uk>
2017-10-13 09:03:54 +0000

    Cron instructions

--- server_monitoring.sh
+++ server_monitoring.sh
@@ -4,6 +4,8 @@
 #You may want to add it in Cron
 #Simple command that keeps log of the date:
 #  sh server_monitoring.sh >> $(date +"%Y%m%d")_server.log
+# In cron:
+# */5 * * * * root sh /home/your_home_directory/server_monitoring.sh >> /home/your_home_directory/$(date +"\%Y\%m\%d")_server.log
 ###############################
 echo "===Beginning==="
 #ping -c 1 google.com && echo "Internet connection: OK" || echo "Internet connection: Down"
