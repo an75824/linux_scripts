a8b738138d6b10536922bde6a0becf73cae3ee3d
antony13 <nanosa@uni.coventry.ac.uk>
2017-10-17 15:35:34 +0100

    various checks before updating the email

--- mahara_scripts/update_email.php
+++ mahara_scripts/update_email.php
@@ -11,20 +11,58 @@ if (!isset($argv[1]) || !isset($argv[2]))
 	exit(1);
 }
 require(dirname(dirname(dirname(__FILE__))) . '/init.php');
-$email='Helloworld@email.dfg';
-$newemail = "Helloworld@email.dfg";
 
-$user = get_record('usr', 'email', $email);
+$current_email = $argv[1];
+$new_email = sanitize_email($argv[2]);
+
+$user = get_record('usr', 'email', $current_email);
 if (!$user)
 {
-	echo 'User does not exist';
-}else {
-	echo $user->email;
-	db_begin();
-	if(update_record('usr', $user))
+	exit("User with email '$current_email' does not exist.\nNothing to update.\n");
+}
+
+$user_id = $user->id;
+
+if (!check_usr_new_email($new_email,$user_id))
+{
+	exit( "$new_email already exists in the 'usr' table\n");
+}
+if (!check_artefact_internal_email($new_email,$user_id))
+{
+	exit("$email already exists in the 'artefact_internal_profile_email' table\n");
+}
+
+update_email($new_email, $user);
+
+function check_usr_new_email($email, $id)
+{
+	$result = record_exists_sql('SELECT id FROM usr WHERE deleted != 1 AND email = ? AND id != ?', array($email, $id));
+	if (!$result)
 	{
-		//set_user_primary_email($user->id, $values['email']);
-		set_user_primary_email($user->id, $newemail);
+		return true; //this means that the there is not any user with this email
+	} else {
+		return false;
+	}
+}
+
+function check_artefact_internal_email($email, $id)
+{
+	$result = record_exists_sql('SELECT owner FROM artefact_internal_profile_email WHERE email = ? AND owner != ?', array($email, $id));
+	if (!$result)
+	{
+		return true; //this means that the there is not any user with this email
+	} else {
+		return false;
+	}
+}
+
+function update_email($newemail,$userObj)
+{
+	db_begin();
+	if(update_record('usr', $userObj))
+	{
+		set_user_primary_email($userObj->id, $newemail);
 		db_commit();
+		echo "User email successfully updated!\n";
 	}
 }
